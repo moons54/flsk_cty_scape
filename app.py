8b6bea83c8f9dfad7d5d9474c552121a77bbaa7e
aurelienmimouni <aurelien.mimouni@gmail.com>
2021-10-03 14:43:00 +0200

    'supp utils'

--- app.py
+++ app.py
@@ -2,16 +2,25 @@ import flask
 from flask import Flask, render_template, jsonify
 import tempfile, os, glob, re
 import json 
-from utils import *
 from keras.preprocessing.image import load_img 
 import requests
 from PIL import Image
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
 import numpy as np
 app = Flask(__name__)
 import urllib.request
 import json
 import os
 import ssl
+import albumentations as A
+A.__version__
+
+batch_size = 20
+data_sample = 3000
+steps = data_sample//batch_size
+img_height, img_width = 256, 256
+x_min = (128 - img_width) // 2
+y_min = (128 - img_height) // 2
 
 
 @app.route("/")
@@ -101,12 +110,6 @@ def prediction(image_id):
 
 @app.route('/<int:image_id>')
 def prediction_fpn(image_id):
-    batch_size = 20
-    data_sample = 3000
-    steps = data_sample//batch_size
-    img_height, img_width = 256, 256
-    x_min = (128 - img_width) // 2
-    y_min = (128 - img_height) // 2
     image_list, mask_list = listImg_Mask('static/image','static/mask')
     it = img_to_array(load_img(f'{image_list[image_id]}', target_size=(img_height, img_width)))/255.
     it_comp = img_to_array(load_img(f'{mask_list[image_id]}', target_size=(img_height, img_width), color_mode='grayscale'))/255.
@@ -151,3 +154,52 @@ def prediction_fpn(image_id):
         url1='/static/prediction/out1.png',
         url2='/static/prediction/out2.png',
         url3='/static/prediction/out3.png')
+
+
+def round_clip_0_1(x, **kwargs):
+    return x.round().clip(0, 1)
+    
+
+
+
+def get_validation_augmentation():
+     
+    """Add paddings to make image shape divisible by 32"""
+    test_transform = [
+        # A.PadIfNeeded(384, 480)
+        A.PadIfNeeded(min_height=x_min, min_width=y_min, always_apply=True, border_mode=0),
+    ]
+    return A.Compose(test_transform)
+
+
+
+# helper function for data visualization
+def visualize(**images):
+    """PLot images in one row."""
+    n = len(images)
+    plt.figure(figsize=(16, 5))
+    for i, (name, image) in enumerate(images.items()):
+        plt.subplot(1, n, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(' '.join(name.split('_')).title())
+        plt.imshow(image)
+    plt.show()
+
+# helper function for data visualization    
+def denormalize(x):
+    """Scale image to range 0..1 for correct plot"""
+    x_max = np.percentile(x, 98)
+    x_min = np.percentile(x, 2)    
+    x = (x - x_min) / (x_max - x_min)
+    x = x.clip(0, 1)
+    return x
+
+
+def listImg_Mask(path_image,path_masks):
+  image_list = [f for f in glob.iglob(os.path.join(path_image, "*.png"))]
+  mask_list = [f for f in glob.iglob(os.path.join(path_masks, "*.png"))]
+  image_list.sort()
+  mask_list.sort()
+ # print(f'. . . . .Number of images: {len(image_list)}\n. . . . .Number of masks: {len(mask_list)}')
+  return image_list,mask_list
